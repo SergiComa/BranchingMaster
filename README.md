bc88993ba74e2a2c6e6edec8f4047bad8b031e7d
Alejandro Bolano <elbola.cuba1992@gmail.com>
2019-06-21 15:59:52 +0200

    [Master] First commit - tuto branching and pull request

---
+++ README.md
@@ -0,0 +1,8 @@
+# FileManager
+Step to step...
+Tutorial of Branching with Pull Request
+
+
+
+File Manager of Windows ✌
+With ❤️ by [Alejandro Bolano](https://github.com/alejandrobolano) 😊
---
+++ Tutorial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager.Common.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public String Name { get; set; }
+        public String Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Student student &&
+                   StudentId == student.StudentId &&
+                   Name == student.Name &&
+                   Surname == student.Surname &&
+                   DateOfBirth == student.DateOfBirth;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -480371866;
+            hashCode = hashCode * -1521134295 + StudentId.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Name);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Surname);
+            hashCode = hashCode * -1521134295 + DateOfBirth.GetHashCode();
+            return hashCode;
+        }
+
+    }
+}
